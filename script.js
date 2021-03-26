128c24171e5ac63cdcf1bc80797f843d27638188
anggiie <anggiie02>
2021-03-26 18:02:44 +0100

    add script

---
+++ script.js
@@ -0,0 +1,26 @@
+const allRonds = document.querySelectorAll('.rond');
+const allBoxes = document.querySelectorAll('.box');
+
+const controller = new ScrollMagic.Controller()
+
+allBoxes.forEach(box => {
+
+    for(i = 0; i < allRonds.length; i++){
+
+        if(allRonds[i].getAttribute('data-anim') === box.getAttribute('data-anim')){
+
+            let tween = gsap.from(box, {y: -50, opacity: 0, duration: 0.5})
+
+            let scene = new ScrollMagic.Scene({
+                triggerElement: allRonds[i],
+                reverse: true
+            })
+            .setTween(tween)
+            // .addIndicators()
+            .addTo(controller)
+
+        }
+
+    }
+
+})
